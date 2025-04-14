81b4696acf36598840e6efceecaf3b9c2027e292
renegatuya <ndore@food.dtu.dk>
2025-04-14 21:43:59 +0200

    Update predict_on_unknown.py

--- predict_on_unknown.py
+++ predict_on_unknown.py
@@ -2,22 +2,20 @@
 import pandas as pd
 import joblib
 
-# Load model and features
+# load model and features
 model = joblib.load("best_amr_model.pkl")
 model_features = joblib.load("model_features.pkl")
 
-# Load and process new data
+# load and process new data
 df = pd.read_csv("/Users/ndore/Academics/PhD/courses/python_1/assignements/amr_prediction_project/outputs/synthetic_git_amr_data.csv")
 
 # Categorical variables to numeric values using one-hot encoding
 df_encoded = pd.get_dummies(df, columns=["country", "district", "pathogen"], drop_first=True)
 
-#Matching the data
+# Matching the data
 X_new = df_encoded.reindex(columns=model_features, fill_value=0)
 
 # Predictions based on the new data
 predictions = model.predict(X_new)
-
-# Print the predictions
 print(predictions)
 
