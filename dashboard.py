d9ba08907adbeb1ffc6992e4f73d63503c9283b2
renegatuya <ndore@food.dtu.dk>
2025-04-14 21:42:11 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,11 +7,9 @@ from PIL import Image
 # Model and figures
 model = joblib.load('best_amr_model.pkl')
 model_comparison_img = Image.open('/Users/ndore/Academics/PhD/courses/python_1/assignements/amr_prediction_project/outputs/model_comparison.png')
-
-# Streamlit Dashboard
 st.title("AMR Prediction Dashboard")
 
-# Display model comparison figure
+# model comparison figure
 st.subheader("Model Comparison")
 st.image(model_comparison_img, caption='Model Comparison')
 
@@ -20,13 +18,9 @@ uploaded_file = st.file_uploader("Choose a CSV file for prediction", type="csv")
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     st.write("Data preview:", df.head())
-
-    #  model predictions
     predictions = model.predict(df) 
     st.subheader("Predictions")
     st.write(predictions)
-
-    # visualise
     st.subheader("Predictions Over Time")
     plt.figure(figsize=(10, 6))
     plt.plot(predictions)
